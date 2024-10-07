76a24f9fd38a9ce6f7a25f397cb64d95521e6d5f
henryJ099123 <henryjochan@gmail.com>
2024-10-07 19:20:46 -0400

    radio_comms -- working on the zmq video transmission

--- radio_comms/practice_files/client_zmq.py
+++ radio_comms/practice_files/client_zmq.py
@@ -1,17 +1,30 @@
 import cv2
 import zmq
+import pickle
 
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
+socket = context.socket(zmq.PUB)
+
+cap = cv2.VideoCapture(0)
 
 print("Looking for server...")
 
 socket.connect("tcp://localhost:12345")
 
-while True:
-    message = input("--Enter message: ")
-    print("Sending request {0}".format(message))
-    socket.send(message.encode())
+print("connected to server!")
 
-    received_message = socket.recv()
-    print("Received {0} from server".format(received_message))
+while True:
+    try:
+        ret, frame = cap.read()
+        #data = pickle.dumps(frame)
+        #print("Sending request {0}".format(message))
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        socket.send(buffer)
+        #received_message = socket.recv()
+        #received_message = received_message.decode()
+        #print("Received {0} from server".format(received_message))
+    except KeyboardInterrupt:
+        cap.release()
+        cv2.destroyAllWindows()
+        print("keyboard interruption")
+        break
--- radio_comms/practice_files/server_zmq.py
+++ radio_comms/practice_files/server_zmq.py
@@ -1,13 +1,27 @@
 import zmq
+import cv2
+import pickle
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket = context.socket(zmq.SUB)
 socket.bind("tcp://*:12345")
 
 print("waiting on receiving info...")
 
 while True:
     message = socket.recv()
-    message = message.decode()
-    print(message)
-    socket.send_string("Got your message!")
+    #frame = pickle.loads(message)
+    #return_val = message
+    frame = cv2.imdecode(message, 1)
+
+    cv2.imshow('frame',frame)
+    cv2.waitKey(5)
+
+    try:
+        pass
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+        print("keyboard interruption")
+        break
+    #print(message)
+    #socket.send_string("Got your message!")
