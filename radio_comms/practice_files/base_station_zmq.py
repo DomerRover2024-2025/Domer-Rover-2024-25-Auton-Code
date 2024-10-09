aa7171087c0425f58ccbd9c9a52bed4842c54bac
henryJ099123 <henryjochan@gmail.com>
2024-10-08 20:33:30 -0400

    radio_comms -- cleaning up

--- radio_comms/practice_files/base_station_zmq.py
+++ radio_comms/practice_files/base_station_zmq.py
@@ -9,6 +9,7 @@ cap = cv2.VideoCapture(0)
 
 HOST = "*"
 PORT = 12346
+QUALITY = 50
 
 print("Looking for server...")
 
@@ -22,11 +23,11 @@ while True:
     #data = pickle.dumps(frame)
     #print("Sending request {0}".format(message))
     if ret:
-        #encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-        encoded, buffer = cv2.imencode('.jpg', frame)
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), QUALITY]
+        encoded, buffer = cv2.imencode('.jpg', frame, encode_param)
     #message = buffer.tobytes()
         socket.send(buffer)
-    time.sleep(1)
+    #time.sleep(1)
 cap.release()
 cv2.destroyAllWindows()
 socket.close()
