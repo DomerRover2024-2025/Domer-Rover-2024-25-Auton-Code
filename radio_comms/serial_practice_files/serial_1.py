7bd3e1bbfadf11743e34d580ee5722e821afef1d
henryJ099123 <henryjochan@gmail.com>
2024-11-03 15:53:12 -0500

    radio_comms -- working on serial connection

--- radio_comms/serial_practice_files/serial_1.py
+++ radio_comms/serial_practice_files/serial_1.py
@@ -4,7 +4,7 @@ port = "/dev/cu.usbserial-B001VC58"
 baud = 57600
 timeout = 3
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
-ser.write("Hello world!")
+ser.write(b'Hello world!')
 ser.flushInput()
 ser.flushOutput()
 try:
--- radio_comms/serial_practice_files/serial_2.py
+++ radio_comms/serial_practice_files/serial_2.py
@@ -1,15 +1,13 @@
 import serial
+import time
 
 port = "COM3"
 baud = 57600
 timeout = 3
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
-output = ser.read()
-print(str(output))
 # ser.flushInput()
 # ser.flushOutput()
-try:
-    while(True):
-        pass
-except KeyboardInterrupt:
-    pass
+while(True):
+    time.sleep(1)
+    output = ser.readline()
+    print((output))
