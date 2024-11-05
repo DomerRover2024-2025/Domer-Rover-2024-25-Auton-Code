0228d95b1393bae434cb76dd829239574c847cf8
unknown <DomerRover2024-2025>
2024-11-04 19:38:22 -0500

    radio_comms -- worked on serial connection

--- radio_comms/serial_practice_files/serial_2.py
+++ radio_comms/serial_practice_files/serial_2.py
@@ -1,13 +1,35 @@
 import serial
+import numpy as np
+import cv2
 import time
 
-port = "COM3"
+
+port = "COM4"
 baud = 57600
-timeout = 3
+timeout = 10
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
+# ser.timeout = 2
 # ser.flushInput()
 # ser.flushOutput()
-while(True):
-    time.sleep(1)
-    output = ser.readline()
-    print((output))
+#while(True):
+    #time.sleep(1)
+    #output = ser.readline()
+    #print((output.decode("utf-8")))
+MAXBYTES = 50_000
+while (True):
+    bytes = ser.in_waiting
+    print(str(bytes))
+    b_output = ser.read(MAXBYTES)
+
+    
+    image = np.frombuffer(b_output, dtype=np.uint8)
+    frame = cv2.imdecode(image, 1)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(100)
+    #print(b_output)
+
+
+
+
+
+cv2.destroyAllWindows()
