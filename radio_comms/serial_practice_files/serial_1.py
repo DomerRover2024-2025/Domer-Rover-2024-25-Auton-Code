c965256abee1f6970fccbfa1741ead7fcd5fb2dd
henryJ099123 <henryjochan@gmail.com>
2024-11-04 19:39:50 -0500

    radio_comms -- workingon sreila connectioN

--- radio_comms/serial_practice_files/serial_1.py
+++ radio_comms/serial_practice_files/serial_1.py
@@ -1,14 +1,23 @@
 import serial
+import cv2
+import sys
 
-port = "/dev/cu.usbserial-B001VC58"
+port = "/dev/cu.usbserial-BG00HO5R"
 baud = 57600
 timeout = 3
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
-ser.write(b'Hello world!')
-ser.flushInput()
-ser.flushOutput()
-try:
-    while(True):
-        pass
-except KeyboardInterrupt:
-    pass
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 10]
+encoded, buffer = cv2.imencode('.jpg', frame, encode_param)
+print(sys.getsizeof(buffer))
+#print(sys.getsizeof(int(sys.getsizeof(buffer))))
+#ser.write(sys.getsizeof(buffer))#
+
+ser.write(buffer)
+#ser.write(b'hadkfakdf\n')
+
+print(ser.in_waiting)
+print(ser.out_waiting)
+
+ser.close()
