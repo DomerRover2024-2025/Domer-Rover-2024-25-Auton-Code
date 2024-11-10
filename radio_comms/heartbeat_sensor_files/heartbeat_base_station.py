c36ca58de3c95b31d522d6996e21e1e78e06f250
henryJ099123 <henryjochan@gmail.com>
2024-11-10 15:30:15 -0500

    radio_comms -- working on serial connection

--- radio_comms/heartbeat_sensor_files/heartbeat_base_station.py
+++ radio_comms/heartbeat_sensor_files/heartbeat_base_station.py
@@ -1,7 +1,9 @@
 import serial
+import struct
+
 
 # specify port, baud, timeout, and create serial object
-port = "/dev/ttys047"
+port = "/dev/cu.usbserial-BG00HO5R"
 baud = 57600
 timeout = 5
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
@@ -9,11 +11,13 @@ ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
 # should receive a heartbeat every second
 try:
     while True:
-        try:
-            output = ser.readline()
-            print(output)
-            print("Heartbeat received.")
-        except TimeoutError as e:
-            print("Lost the rover. Continuing to receive messages...")
+        output = ser.read(1)
+        if output != b'':
+            real_output = struct.unpack("B", output)
+            print(real_output[0])
+        
+        #     print(output, "Heartbeat received.")
+        # else:
+        #     print("Lost the rover. Continuing to receive messages...")
 except:
     ser.close()
