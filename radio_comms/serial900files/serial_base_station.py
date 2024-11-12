3cc8929c018ba764da5b812236dfa1d71441bc8e
unknown <DomerRover2024-2025>
2024-11-11 19:58:07 -0500

    radio_comms -- working with the serial connection

--- radio_comms/serial900files/serial_base_station.py
+++ radio_comms/serial900files/serial_base_station.py
@@ -1,3 +1,6 @@
+# This is the client side of a client-server model.
+# This will be sending requests to the rover.
+
 import serial
 import numpy as np
 import cv2
@@ -5,10 +8,13 @@ import time
 import struct
 
 #port = "/dev/cu.usbserial-BG00HO5R"
-port = "COM4"
+port = "COM3"
 baud = 57600
 timeout = 15
 ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
+
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 
 MAXBYTES = 50_000
 while (True):
@@ -16,10 +22,25 @@ while (True):
     request = int(request)
     ser.write(struct.pack("B", request))
 
-    if request == 1:
+    if request == 0: # end connection
+        print("Request to quit sent.")
+
+        ack = struct.unpack("B", ser.read(1))
+        if ack[0] == 1: # successful connection break
+            print("Request successful.")
+            break
+        else:
+            print("Request unsucessful.")
+
+    if request == 1: # photo
         gotimage = False
+        while True:
+            size_of_image = ser.read(4)
+            if size_of_image != b'':
+                size_of_image = struct.unpack("L", size_of_image)[0]
+                break
         while not gotimage:
-            b_output = ser.read(MAXBYTES)
+            b_output = ser.read(size_of_image)
 
             if b_output != b'':
                 print("received image")
