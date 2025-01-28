a51ba8dda5df976b1d5455fa9ebe3ff20c9febc6
henryJ099123 <henryjochan@gmail.com>
2025-01-28 18:54:41 -0500

    finished testing :D

---
+++ radio_comms/hieroglyphics/tester_1.py
@@ -0,0 +1,18 @@
+import serial
+from message import Message
+from functools import reduce
+
+ser = serial.Serial("/dev/tty.usbserial-B001VC58", baudrate=57600, timeout=0.1)
+
+string = "please work"
+
+msg = Message(purpose=3, payload=string.encode())
+print(msg.payload[0] ^ msg.payload[1])
+print(bytes(reduce(lambda a,b: a ^ b, msg.payload)))
+print(msg.checksum)
+
+while True:
+    if input(">>") == "go":
+        print(msg)
+        print(msg.get_as_bytes())
+        ser.write(msg.get_as_bytes())
