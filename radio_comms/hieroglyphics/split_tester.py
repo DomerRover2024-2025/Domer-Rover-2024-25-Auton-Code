5cadc2fade377cf58a18c6ded619a504d44e4259
jshade1914 <144067033+jshade1914@users.noreply.github.com>
2025-02-04 20:03:08 -0500

    split tester file

--- radio_comms/hieroglyphics/split_tester.py
+++ radio_comms/hieroglyphics/split_tester.py
@@ -1,19 +1,26 @@
-import serial
+#import serial
 from message import Message
 from functools import reduce
 
-ser = serial.Serial("/dev/tty.usbserial-B001VC58", baudrate=57600, timeout=0.1)
+#ser = serial.Serial("/dev/tty.usbserial-B001VC58", baudrate=57600, timeout=0.1)
 
-string = ['h' for i in range(60000)]
+string = [0xef for i in range(9000)]
 
-msg = Message(purpose=3, payload=string.encode())
+string = bytearray(string)
+
+msg = Message(purpose=3, payload=string)
 print(msg.payload[0] ^ msg.payload[1])
-print(bytes(reduce(lambda a,b: a ^ b, msg.payload)))
+print(msg.payload)
+print()
 print(msg.checksum)
-msg_list = msg.message_split(msg.get_as_bytes())
+msg_list = msg.message_split(msg.payload,3)
 
 current_msg = Message()
 
 for list_msgs in msg_list:
-    current_msg = current_msg.convert_from_bytestring(current_msg, list_msgs)
-    print(current_msg.payload[0] ^ current_msg.payload[1])
+    print()
+    print(list_msgs.payload[0] ^ list_msgs.payload[1])
+    print()
+    print(list_msgs.payload)
+    print()
+    print(list_msgs.checksum)
